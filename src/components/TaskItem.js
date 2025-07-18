5dff648abc520ba2ccf1297d45f8cc31b37ef8e1
Guilherme Schumann <gui.rsc45@gmail.com>
2025-07-18 14:19:58 -0300

    feat: fetch tasks from backend; clear useless files

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,14 +1,4 @@
-import { useEffect } from "react";
-
 const TaskItem = ({ task }) => {
-    useEffect(() => {
-        console.log("Component was mounted!");
-
-        return () => {
-            console.log("I will unmount!");
-        };
-    }, []);
-
     return (
         <>
             <h1>{task.description}</h1>
@@ -18,25 +8,3 @@ const TaskItem = ({ task }) => {
 };
 
 export default TaskItem;
-
-// import React from "react";
-// class TaskItem extends React.Component {
-//     componentDidMount() {
-//         console.log("Component is mounted!");
-//     }
-
-//     componentWillUnmount() {
-//         console.log("I will unmount!");
-//     }
-//     render() {
-//         const { task } = this.props;
-//         return (
-//             <>
-//                 <h1>{task.description}</h1>
-//                 <p>{task.isCompleted ? "Completa" : "Não Completa"}</p>
-//             </>
-//         );
-//     }
-// }
-
-// export default TaskItem;
