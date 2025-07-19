640780fa51ebd534bd6c027c82857c24c497988b
Guilherme Schumann <gui.rsc45@gmail.com>
2025-07-19 16:00:08 -0300

    feat: add CustomInput component

---
+++ src/components/CustomInput.jsx
@@ -0,0 +1,19 @@
+import "./CustomInput.scss";
+
+const CustomInput = ({ label }) => {
+    return (
+        <div className="custom-input-container">
+            <input type="text" className="custom-input" />
+
+            {label ? (
+                <label
+                    className={`${
+                        value.length != "" ? "shrink" : ""
+                    } custom-input-label`}
+                ></label>
+            ) : null}
+        </div>
+    );
+};
+
+export default CustomInput;
