c99605c6ed76ee9ba59d2a5440e64fd08d969b4a
Guilherme Schumann <gui.rsc45@gmail.com>
2025-07-21 19:49:58 -0300

    feat: add function to add a task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { toast } from "react-toastify";
+import axios from "axios";
 
 import CustomInput from "./CustomInput";
 import CustomButton from "./CustomButton";
 
 import "./AddTask.scss";
 
-const AddTask = () => {
+const AddTask = ({fetchTasks}) => {
     const [task, setTask] = useState("");
 
     const onChange = (e) => {
@@ -18,10 +19,23 @@ const AddTask = () => {
         try {
             if (task.length === 0) {
                 return toast.error(
-                        "A tarefa precisa de uma descrição para ser adicionada"
+                    "A tarefa precisa de uma descrição para ser adicionada"
                 );
             }
-        } catch (error) {}
+
+            await axios.post("http://localhost:8000/tasks", {
+                description: task,
+                isCompleted: false,
+            });
+
+            await fetchTasks();
+
+            setTask("");
+
+            await toast.success("A tarefa foi adicionada com sucesso!");
+        } catch (error) {
+            toast.error("Algo deu errado.");
+        }
     };
 
     return (
@@ -30,9 +44,10 @@ const AddTask = () => {
                 label="Adicionar tarefa ..."
                 value={task}
                 onChange={onChange}
+                onEnterPress={handleTaskAddition}
             />
-            <CustomButton>
-                <FaPlus size={14} color="#FFFFFF" onClick={handleTaskAddition}/>
+            <CustomButton onClick={handleTaskAddition}>
+                <FaPlus size={14} color="#FFFFFF" />
             </CustomButton>
         </div>
     );
